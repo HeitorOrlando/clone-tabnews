0eab34a72b037660302fab1137dc7f260139072d
Heitor Orlando(F0xTrot) <heitororlandoms@gmail.com>
2024-12-12 21:49:46 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório para aprender a fazer do Zero um projeto
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1><strong>Site em construção</strong></h1>
+function Home() {
+  return (
+    <h1>
+      <strong>Site em construção</strong>
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("dawdawd");
 }
 
 export default Home;
